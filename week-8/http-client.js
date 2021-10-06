89e2dfd7af8120ea1612083423859c472b3d7349
Lucas Hoffman <75821031+lucash0331@users.noreply.github.com>
2021-10-06 10:45:32 -0400

    Add files via upload

--- week-8/http-client.js
+++ week-8/http-client.js
@@ -7,21 +7,13 @@
 ;===========================================
 */
 
-// HTTP Client class.
 export class HttpClient {
-    async get(url, params = "") {
-      // New URL object
-      let urlObject = new URL(url);
-  
-      // Assigning url.search property to URLSearchParams object
-      let paramsObject = new URLSearchParams(params);
-      urlObject.search = paramsObject;
-  
-      const res = await fetch(urlObject.toString(), {
-        method: "GET",
-      });
-  
-      // Return res object as JSON
-      return res.json();
-    }
+  constructor() {}
+
+  async get(url, params = "") {
+    url = new URL(url);
+    url.search = new URLSearchParams(params);
+    let res = await fetch(url.toString(), { method: "GET" });
+    return res.json();
   }
+}
