536d9a341f44ca418e03de38f0b7c828e2dba0f0
Lucas Hoffman <75821031+lucash0331@users.noreply.github.com>
2021-10-06 10:45:43 -0400

    Add files via upload

---
+++ http-client.js
@@ -0,0 +1,19 @@
+/*
+============================================
+;  Title:  http-client.js
+;  Author: Lucas Hoffman
+;  Date: October 5, 2021
+;  Description: JS file for whatabook2 
+;===========================================
+*/
+
+export class HttpClient {
+  constructor() {}
+
+  async get(url, params = "") {
+    url = new URL(url);
+    url.search = new URLSearchParams(params);
+    let res = await fetch(url.toString(), { method: "GET" });
+    return res.json();
+  }
+}
