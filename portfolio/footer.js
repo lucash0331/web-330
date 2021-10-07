435f43d7216394944cf3ead1a9581e95024984e0
Lucas Hoffman <75821031+lucash0331@users.noreply.github.com>
2021-10-07 00:49:38 -0400

    Add files via upload
    Adding content for personal portfolio assignment

---
+++ portfolio/footer.js
@@ -0,0 +1,36 @@
+/*
+Title: footer.js
+Author: Lucas Hoffman
+Date: 10/06/2021
+Description: footer.js file for personal portfolio
+*/
+
+
+
+
+class Footer extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+              <div id="footer">
+                  <p class="copyright">
+                      Copyright &copy; 2021. Lucas Hoffman |  <a target="_blank" href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/">Bellevue University Web Development</a>
+                  </p>
+                  <p class="social">   
+                      <a target="_blank" class="youtube" title="Course's YouTube channel" href="https://www.youtube.com/user/bellevueuniversity"><i class="fa fa-fw fa-2x fa-inverse fa-youtube"></i></a>
+                      <a target="_blank" class="github" title="My GitHub repositories" href="https://github.com/lucash0331"><i class="fa fa-fw fa-2x fa-inverse fa-github"></i></a>
+                      <a target="_blank" class="github" title="Courses GitHub repositories" href="https://github.com/buwebdev/web-330"><i class="fa fa-fw fa-2x fa-inverse fa-github"></i></a>
+                      <a target="_blank" class="instagram" title="Bellevue University's Instagram" href="https://www.instagram.com/bellevueuniversity/"><i class="fa fa-fw fa-2x fa-inverse fa-instagram"></i></a>
+                      <a target="_blank" class="twitter" title="Bellevue University's Twitter" href="https://www.twitter.com/BellevueU/"><i class="fa fa-fw fa-2x fa-inverse fa-twitter"></i></a>
+                      <a target="_blank" class="facebook" title="Bellevue University's Facebook" href="https://www.facebook.com/BellevueUniversity/"><i class="fa fa-fw fa-2x fa-inverse fa-facebook"></i></a>
+                  </p>
+              </div>
+            
+          `;
+    }
+  }
+  
+  customElements.define("footer-component", Footer);
---
+++ portfolio/header.js
@@ -0,0 +1,31 @@
+/*
+Title: header.js
+Author: Lucas Hoffman
+Date: 10/06/2021
+Description: header.js file for personal portfolio
+*/
+
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+      <div class="cbp-af-header">
+          <div class="cbp-af-inner">
+              <h1>Lucas Hoffman</h1>
+              <nav>
+                  <a href="index.html">Home</a>
+                  <a href="about.html">About</a>
+                  <a href="projects.html">Projects</a>
+                  <a href="devops.html">DevOps</a>
+                  <a href="resume.html">Resume</a>
+              </nav>
+          </div>
+      </div>
+          `;
+    }
+  }
+  
+  customElements.define("header-component", Header);
