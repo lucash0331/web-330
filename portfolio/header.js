1bacd8f70d016de87233c7f80445e0876f5b893a
Lucas Hoffman <75821031+lucash0331@users.noreply.github.com>
2022-01-26 11:29:04 -0500

    Update header.js

--- portfolio/header.js
+++ portfolio/header.js
@@ -1,36 +1,4 @@
-<<<<<<< HEAD
-/*
-Title: header.js
-Author: Lucas Hoffman
-Date: 10/06/2021
-Description: header.js file for personal portfolio
-*/
 
-class Header extends HTMLElement {
-    constructor() {
-      super();
-    }
-  
-    connectedCallback() {
-      this.innerHTML = `
-      <div class="cbp-af-header">
-          <div class="cbp-af-inner">
-              <h1>Lucas Hoffman</h1>
-              <nav>
-                  <a href="index.html">Home</a>
-                  <a href="about.html">About</a>
-                  <a href="projects.html">Projects</a>
-                  <a href="resume.html">Resume</a>
-                  <a href="hoffman-diagrams.html">Diagrams</a>
-              </nav>
-          </div>
-      </div>
-          `;
-    }
-  }
-  
-  customElements.define("header-component", Header);
-=======
 /*
 Title: header.js
 Author: Lucas Hoffman
@@ -63,4 +31,4 @@ class Header extends HTMLElement {
   }
   
   customElements.define("header-component", Header);
->>>>>>> 4fa8f3d93e75a6878f3c6e8b501b24a6ed4f4c03
+
