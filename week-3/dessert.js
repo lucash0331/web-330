f30f32f12cffc3ada8676649a8a83f3e9c643fb0
Lucas Hoffman <75821031+lucash0331@users.noreply.github.com>
2022-01-26 11:14:39 -0500

    Update dessert.js

--- week-3/dessert.js
+++ week-3/dessert.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
 	Title: dessert.js
 	Author: Lucas Hoffman
@@ -16,23 +16,4 @@ export class Dessert extends Product {
   constructor(name, price) {
     super(name, price);
   }
-=======
-/*
-	Title: dessert.js
-	Author: Lucas Hoffman
-	Date: 8/27/2021
-	Description: JavaScript file for dessert
-	Link to GitHub repository: https://github.com/lucash0331/web-330
-	Link to Landing page: https://lucash0331.github.io/web-330/
-*/
-
-//Importing product class from product.js fileS
-import { Product } from "./product.js";
-
-//Exporting and declaring Dessert class & defining its properties.
-export class Dessert extends Product {
-  constructor(name, price) {
-    super(name, price);
-  }
->>>>>>> 4fa8f3d93e75a6878f3c6e8b501b24a6ed4f4c03
 }
