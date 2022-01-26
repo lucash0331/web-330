c8e523a38a4f4a733916b83228117080b80126d0
Lucas Hoffman <75821031+lucash0331@users.noreply.github.com>
2022-01-26 11:17:20 -0500

    Update main-course.js

--- week-3/main-course.js
+++ week-3/main-course.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
 	Title: main-course.js
 	Author: Lucas Hoffman
@@ -16,23 +16,4 @@ export class MainCourse extends Product {
   constructor(name, price) {
     super(name, price);
   }
-=======
-/*
-	Title: main-course.js
-	Author: Lucas Hoffman
-	Date: 8/27/2021
-	Description: JavaScript file for main course
-	Link to GitHub repository: https://github.com/lucash0331/web-330
-	Link to Landing page: https://lucash0331.github.io/web-330/
-*/
-
-//Importing Product from product.js file.
-import { Product } from "./product.js";
-
-//Exporting the Product class and assigning its properties.
-export class MainCourse extends Product {
-  constructor(name, price) {
-    super(name, price);
-  }
->>>>>>> 4fa8f3d93e75a6878f3c6e8b501b24a6ed4f4c03
 }
