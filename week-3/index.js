fe84290677f92241bc5bc927683640932171e553
Lucas Hoffman <75821031+lucash0331@users.noreply.github.com>
2022-01-26 11:16:53 -0500

    Update index.js

--- week-3/index.js
+++ week-3/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
 	Title: index.js
 	Author: Lucas Hoffman
@@ -15,22 +15,4 @@ export * from "./appetizer.js";
 export * from "./beverage.js";
 export * from "./dessert.js";
 export * from "./main-course.js";
-=======
-/*
-	Title: index.js
-	Author: Lucas Hoffman
-	Date: 8/27/2021
-	Description: JavaScript file for index.js
-	Link to GitHub repository: https://github.com/lucash0331/web-330
-	Link to Landing page: https://lucash0331.github.io/web-330/
-*/
-
-//exporting all of the JS classes to index file
-
-//Exporting js files.
-export * from "./appetizer.js";
-export * from "./beverage.js";
-export * from "./dessert.js";
-export * from "./main-course.js";
->>>>>>> 4fa8f3d93e75a6878f3c6e8b501b24a6ed4f4c03
 export * from "./bill.js";
