bfabbe4a6c15d5d3bdb654595055969e20946647
Lucas Hoffman <75821031+lucash0331@users.noreply.github.com>
2022-01-26 11:14:13 -0500

    Update bill.js

--- week-3/bill.js
+++ week-3/bill.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
 	Title: bill.js
 	Author: Lucas Hoffman
@@ -57,64 +57,5 @@ export class Bill {
 
     return total.toFixed(2);
   }
-=======
-/*
-	Title: bill.js
-	Author: Lucas Hoffman
-	Date: 8/27/2021
-	Description: JavaScript file for bill
-	Link to GitHub repository: https://github.com/lucash0331/web-330
-	Link to Landing page: https://lucash0331.github.io/web-330/
-*/
 
-//Exporting bill class & defining its properties.
-export class Bill {
-  //Assigning to empty arrays.
-  constructor(_beverages, _appetizers, _mainCourses, _desserts) {
-    this._beverages = [];
-    this._appetizers = [];
-    this._mainCourses = [];
-    this._desserts = [];
-  }
-
-  //functions that will add specific items.
-  addBeverage(beverage) {
-    this._beverages.push(beverage);
-  }
-
-  addAppetizer(appetizer) {
-    this._appetizers.push(appetizer);
-  }
-
-  addMainCourse(mainCourse) {
-    this._mainCourses.push(mainCourse);
-  }
-
-  addDessert(dessert) {
-    this._desserts.push(dessert);
-  }
-
-  //Get total function that will take the chosen items and add to the total price.
-  getTotal() {
-    let total = 0;
-
-    let beverageTotal = this._beverages.forEach(function (beverage) {
-      total += parseFloat(beverage.price);
-    });
-
-    let appetizerTotal = this._appetizers.forEach(function (appetizer) {
-      total += parseFloat(appetizer.price);
-    });
-
-    let mainCourse = this._mainCourses.forEach(function (mainCourse) {
-      total += parseFloat(mainCourse.price);
-    });
-
-    let dessertTotal = this._desserts.forEach(function (dessert) {
-      total += parseFloat(dessert.price);
-    });
-
-    return total.toFixed(2);
-  }
->>>>>>> 4fa8f3d93e75a6878f3c6e8b501b24a6ed4f4c03
 }
